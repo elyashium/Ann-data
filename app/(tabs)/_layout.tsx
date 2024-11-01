820bca7faaad863b08b9c7fa0d17a8c6ba37cf65
Shash <ashishsinghlogic@gmail.com>
2024-11-01 20:34:36 +0530

    botttom bar gradient

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,18 +2,30 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarStyle: {
+          backgroundColor: '#4CAF50', // Your desired background color
+          borderTopWidth: 0, // Optional: remove top border
+          height: 65, // Optional: adjust height
+        },
+        tabBarActiveTintColor: "#001700",
+        tabBarInactiveTintColor: '#ffffff',
+        tabBarActiveBackgroundColor: '#74d678',
+
+        // tabBarItemStyle: {
+        //   borderRadius: 10, // Optional: rounded corners
+        // },
         headerShown: false,
       }}>
+
       <Tabs.Screen
         name="index"
         options={{
@@ -28,7 +40,10 @@ export default function TabLayout() {
         options={{
           title: 'Profile',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "person" : "person-outline"}
+              color={ color}
+            />
           ),
         }}
       />
