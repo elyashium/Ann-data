134dc14224e8d144f5a0e924c0f85fa1ef680285
Shash <ashishsinghlogic@gmail.com>
2024-11-02 23:22:44 +0530

    Update profile.tsx

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,58 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from "react-native";
+
 
 
 export default function profile() {
   return (
-    <SafeAreaView>
-      <Text>
-        profile page
-      </Text>
-    </SafeAreaView>
+
+
+    <View style={styles.container}>
+
+
+      {/* for username top half*/}
+
+      <View style={styles.upperhalf}>
+
+        <View style={styles.username}>
+
+          <View>
+            {/* for username photo */}
+          </View>
+          <Text> User Name </Text>
+          
+        </View>
+
+      </View>
+
+
+
+      {/* lower half */}
+      <View>
+        <View>
+          {/* the details box */}
+        </View>
+      </View>
+
+
+
+    </View>
   )
 }
 
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+
+  upperhalf: {
+    backgroundColor: '#4CAF50',
+    height: 200
+  },
+  username :{
+
+  }
+
+})
